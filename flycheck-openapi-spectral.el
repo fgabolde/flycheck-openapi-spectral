decdc539ed2652cdf90c7b5b5c20df229d8fccf5
Fabrice Gabolde <fabrice.gabolde@gmail.com>
2020-05-05 14:31:30 +0200

    flycheck-openapi-spectral.el

---
+++ flycheck-openapi-spectral.el
@@ -0,0 +1,24 @@
+;;; flycheck-openapi-spectral.el -- Support for flycheck in Swagger 2/OpenAPI 3 buffers using Spectral
+
+;; -*- coding: utf-8 -*-
+
+(flycheck-define-checker openapi-spectral
+  "A Swagger/OpenAPI linter using Spectral.
+
+See `https://github.com/stoplightio/spectral'."
+  :command ("spectral" "lint" "-f" "text" source)
+  :error-patterns ((warning line-start (file-name) ":" line ":" column " warning " (id (one-or-more (not blank))) " " (message))
+                   (error line-start (file-name) ":" line ":" column " error " (id (one-or-more (not blank))) " " (message)))
+  :modes yaml-mode
+  :predicate (lambda ()
+               (or
+                (string-match
+                 "swagger\\([[:space:]]\\)*:[[:space:]]*[\\\"']?2.0[\\\"']?"
+                 ;; Need to avoid stack overflow for multi-line regex
+                 (buffer-substring 1 (min (buffer-size) 300)))
+                (string-match
+                 "openapi\\([[:space:]]\\)*:[[:space:]]*[\\\"']?3.*[\\\"']?"
+                 ;; Need to avoid stack overflow for multi-line regex
+                 (buffer-substring 1 (min (buffer-size) 300))))))
+
+(add-to-list 'flycheck-checkers 'openapi-spectral)
