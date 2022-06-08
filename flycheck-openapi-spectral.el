852889dd0c6a8b15c53a0e3e0d33966c3b0a74e2
Fabrice Gabolde <fgabolde@weborama.com>
2022-06-08 16:34:34 +0200

    Explicitly use --ruleset when a common spectral config file exists.

--- flycheck-openapi-spectral.el
+++ flycheck-openapi-spectral.el
@@ -6,7 +6,7 @@
   "A Swagger/OpenAPI linter using Spectral.
 
 See `https://github.com/stoplightio/spectral'."
-  :command ("spectral" "lint" "-f" "text" source)
+  :command ("spectral" "lint" "-f" "text" (config-file "--ruleset" flycheck-openapi-spectralyml) source)
   :error-patterns ((warning line-start (file-name) ":" line ":" column " warning " (id (one-or-more (not blank))) " " (message))
                    (error line-start (file-name) ":" line ":" column " error " (id (one-or-more (not blank))) " " (message)))
   :modes yaml-mode
@@ -20,5 +20,8 @@ See `https://github.com/stoplightio/spectral'."
                  "openapi\\([[:space:]]\\)*:[[:space:]]*[\\\"']?3.*[\\\"']?"
                  ;; Need to avoid stack overflow for multi-line regex
                  (buffer-substring 1 (min (buffer-size) 300))))))
+
+(flycheck-def-config-file-var flycheck-openapi-spectralyml openapi-spectral
+                              '(".spectral.yml" ".spectral.yaml" ".spectral.json"))
 
 (add-to-list 'flycheck-checkers 'openapi-spectral)
