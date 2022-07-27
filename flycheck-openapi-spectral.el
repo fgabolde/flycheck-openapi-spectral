5da11316e987592347982014faa4ac31b30ec2e3
Fabrice Gabolde <fgabolde@weborama.com>
2022-07-27 17:53:09 +0200

    Package header

--- flycheck-openapi-spectral.el
+++ flycheck-openapi-spectral.el
@@ -1,6 +1,27 @@
-;;; flycheck-openapi-spectral.el -- Support for flycheck in Swagger 2/OpenAPI 3 buffers using Spectral
+;;; flycheck-openapi-spectral.el --- Support for flycheck in Swagger 2/OpenAPI 3 buffers using Spectral  -*- lexical-binding: t; coding: utf-8 -*-
 
-;; -*- coding: utf-8 -*-
+;; Copyright (C) 2022  Fabrice Gabolde
+
+;; Author: Fabrice Gabolde <fgabolde@BIG02-fgabolde>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Requires https://github.com/stoplightio/spectral to be installed.
+
+;;; Code:
 
 (flycheck-define-checker openapi-spectral
   "A Swagger/OpenAPI linter using Spectral.
@@ -26,3 +47,6 @@ See `https://github.com/stoplightio/spectral'."
                               '(".spectral.yml" ".spectral.yaml" ".spectral.json"))
 
 (add-to-list 'flycheck-checkers 'openapi-spectral)
+
+(provide 'flycheck-openapi-spectral)
+;;; flycheck-openapi-spectral.el ends here
